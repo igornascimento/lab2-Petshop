6429b2b5bfe8dde3a630039a22a3bf57ace16e4a
Igor Nascimento <igornascimento@gmail.com>
2018-04-09 23:25:29 +0000

    Initializing project.

---
+++ src/lab2/petshop/Lab2Petshop.java
@@ -0,0 +1,16 @@
+package lab2.petshop;
+
+/**
+ *
+ * @author Igor Nascimento <igornascimento@gmail.com>
+ */
+public class Lab2Petshop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
